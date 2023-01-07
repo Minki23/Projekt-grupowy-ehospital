0b6eed85816dae4ba4000037f575d90abdd366f4
olekszczepanowski <olekszczepanowski@wp.pl>
2023-01-07 18:56:00 +0100

    commit zmiany w panelu pacjenta

---
+++ GUI/PatientGraphicInterface.java
@@ -0,0 +1,13 @@
+package GUI;
+
+public class PatientGraphicInterface {
+    private PatientGraphicInterface(){}
+    public static void displayPatientInterface() {
+
+        System.out.println("-----------------------------");
+        System.out.println("| 0: LOG OUT                   |");
+        System.out.println("| 1: SEE MY PROFILE          |");
+        System.out.println("| 2: MAKE AN APPOINTMENT     |");
+        System.out.println("-----------------------------");
+    }
+}
--- UI/PatientInterface.java
+++ UI/PatientInterface.java
@@ -1,17 +1,41 @@
 package UI;
 
+import GUI.AdminGraphicInterface;
+import GUI.PatientGraphicInterface;
 import Model.Patient;
 
 public final class PatientInterface {
 
-    private PatientInterface() {}
+    private PatientInterface() {
+    }
 
     public static void mainInterface() {
 
         UserAccount.patientLogin();
         if (UserAccount.getLoggedPatient() != null) {
-            Patient logged = UserAccount.getLoggedPatient();
-            System.out.println("You are logged as: " + logged);
+            Patient loggedPatient = UserAccount.getLoggedPatient();
+            System.out.println("You are logged as: " + loggedPatient);
+            int choice;
+            do {
+                PatientGraphicInterface.displayPatientInterface();
+                System.out.print(": ");
+                choice = UserAccount.getIntInput();
+                switch (choice) {
+                    case 0 -> {}
+                    case 1 -> displayMyProfile(loggedPatient);
+                    case 2 -> makeAnAppointment(loggedPatient);
+                    default -> throw new IllegalStateException("Unexpected value: " + choice);
+                }
+                System.out.println();
+            } while (choice != 0);
         }
     }
+
+    public static void displayMyProfile(Patient loggedPatient) {
+        System.out.println(loggedPatient);
+    }
+    public static void makeAnAppointment(Patient loggedPatient){
+        System.out.println("Making appointment...");
+    }
+
 }

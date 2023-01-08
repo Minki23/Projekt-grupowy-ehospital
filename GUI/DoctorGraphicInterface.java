cbeeee8851ca927d3df48d1b15845a33637e6ef5
Michal Sosnowski <sosnowskimichal.028@gmail.com>
2023-01-09 00:03:01 +0100

    poprawki bledow

--- GUI/DoctorGraphicInterface.java
+++ GUI/DoctorGraphicInterface.java
@@ -6,12 +6,12 @@ public class DoctorGraphicInterface {
     public static void displayDoctorInterface() {
 
         System.out.println("---------------------------------------");
-        System.out.println("| 0: Exit                                |");
-        System.out.println("| 1: Show my profile                     |");
-        System.out.println("| 2. Edit my profile                     |");
-        System.out.println("| 3: Search for a patient                |");
-        System.out.println("| 4: Show upcoming appointments          |");
-        System.out.println("| 5. Show my opinions                    |");
+        System.out.println("| 0: Exit                             |");
+        System.out.println("| 1: Show my profile                  |");
+        System.out.println("| 2. Edit my profile                  |");
+        System.out.println("| 3: Search for a patient             |");
+        System.out.println("| 4: Show upcoming appointments       |");
+        System.out.println("| 5. Show my opinions                 |");
         System.out.println("---------------------------------------");
     }
 }
